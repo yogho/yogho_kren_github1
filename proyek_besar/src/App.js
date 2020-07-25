8d480e9e3d503248111885fd0e1007c55ffdb80d
FadjarIftikhar <fadjar.iftikhar@gmail.com>
2020-07-25 22:17:28 +0700

    mencari menu tidak mau tampil

--- proyek_besar/src/App.js
+++ proyek_besar/src/App.js
@@ -1,25 +1,50 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {IonButton, IonPage, IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem,IonRouterOutlet } from '@ionic/react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <IonButton onClick color="primary"> Primary
+     <IonPage>
+    <IonMenu side="start" menuId="custom" className="my-custom-menu">
+      <IonHeader>
+        <IonToolbar color="tertiary">
+          <IonTitle>Custom Menu</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonList>
+          <IonItem>Menu Item</IonItem>
+          <IonItem>Menu Item</IonItem>
+          <IonItem>Menu Item</IonItem>
+          <IonItem>Menu Item</IonItem>
+          <IonItem>Menu Item</IonItem>
+        </IonList>
+      </IonContent>
+    </IonMenu>
+
+    <IonMenu side="end" type="push">
+      <IonHeader>
+        <IonToolbar color="danger">
+          <IonTitle>End Menu</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonList>
+          <IonItem>Menu Item</IonItem>
+          <IonItem>Menu Item</IonItem>
+          <IonItem>Menu Item</IonItem>
+          <IonItem>Menu Item</IonItem>
+          <IonItem>Menu Item</IonItem>
+        </IonList>
+      </IonContent>
+    </IonMenu>
+    <IonRouterOutlet></IonRouterOutlet>
+    </IonPage>
+    </IonButton>
+  </>
   );
 }
 
