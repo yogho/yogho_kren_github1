838ddb735be11b432e0d47b6045e6b6752671f0f
FadjarIftikhar <fadjar.iftikhar@gmail.com>
2020-08-08 22:36:52 +0700

    belajar variabel
    string
    number
    array
    object

--- proyek_besar/src/Appcomponent.js
+++ proyek_besar/src/Appcomponent.js
@@ -2,9 +2,23 @@ import React from 'react'
 import {IonButton, IonMenuToggle, IonText, IonButtons, IonPage, IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem,IonRouterOutlet } from '@ionic/react';
 import Tulisan from './Tulisan'
 function Appcomponent() {
-
-
-
+const contohvariabel = {
+    nama : "khanza",
+    ttl : "bekasi 22 januari 2020",
+    photo : process.env.PUBLIC_URL + "large_bonsai_wahong.jpg",
+    contoharray : [" khanza ", " medina ", 2020],
+    contohArrObj : [
+    {
+        subobj:"bent1"
+    },
+    {
+        subobj:"bent2"
+    },
+    {
+        subobj:"bent3"
+    }, 
+]
+}
     return (
 
         <div>
@@ -15,7 +29,9 @@ function Appcomponent() {
      </IonButton>
      
     </IonToolbar>
-   <Tulisan></Tulisan>
+   <Tulisan contoh_props={contohvariabel}>
+   </Tulisan>
+
 
 
 </div>
--- proyek_besar/src/Tulisan.js
+++ proyek_besar/src/Tulisan.js
@@ -1,8 +1,13 @@
 import React from 'react'
 
-function Tulisan() {
+function Tulisan(props) {
     return (
-        <div>
+        <div >
+          <h1>{props.contoh_props.nama}</h1>
+          <h1>{props.contoh_props.ttl}</h1>
+          <img src={props.contoh_props.photo}></img>
+          <h1>{props.contoh_props.contoharray}</h1>
+          <h1>{props.contoh_props.contohArrObj[2].subobj}</h1>
           <ion-text color="danger">
   <h5>H5: The quick brown fox jumps over the lazy dog</h5>
 </ion-text>   
